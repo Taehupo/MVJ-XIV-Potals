a358364dc28022e6a44ee7db370ea431f1fe737e
ptilulu <lucas.moragues@wanadoo.fr>
2022-05-15 20:01:08 +0200

    [code] added base of  CharacterShape enum

---
+++ Assets/Script/Enums/CharacterShape.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShape
+{
+    #region Data Types
+
+    /// <summary>
+    /// Shapes that the main character can turn into
+    /// </summary>
+    public enum ECharacterShape
+    {
+        /// <summary> Default basic Human </summary>
+        Human,
+
+
+        /// <summary> usefull for loops </summary>
+        count,
+    }
+    #endregion
+}
