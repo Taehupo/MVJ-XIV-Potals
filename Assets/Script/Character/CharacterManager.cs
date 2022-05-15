2479dd931046d23d4be3a7b4ca18f057e76dbcd6
ptilulu <lucas.moragues@wanadoo.fr>
2022-05-15 20:11:35 +0200

    [code] added base of CharacterManager

---
+++ Assets/Script/Character/CharacterManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    #region Members
+
+    public static CharacterManager Instance { get; private set; }
+
+
+    //private MovementController MovementController { get; private set; }
+    //private AttackController AttackController { get; private set; }
+    //private ShapeController ShapeController { get; private set; }
+
+    #endregion
+
+
+    #region Public Manipulators
+
+    #endregion
+
+
+    #region Private Manipulators
+
+    #endregion
+}
