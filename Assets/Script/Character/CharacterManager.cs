2721bbf6610aad791c073ae4c1de364fef116748
Aubert Gabriel <uzumakigip@gmail.com>
2022-05-15 22:44:43 +0200

    Started work on input system/Added horizontal move

--- Assets/Script/Character/CharacterManager.cs
+++ Assets/Script/Character/CharacterManager.cs
@@ -1,29 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterManager : MonoBehaviour
 {
     #region Members
 
     public static CharacterManager Instance { get; private set; }
+	public float CharacterSpeed { get => characterSpeed; set => characterSpeed = value; }
+
+	MovementController movementController;
+	AttackController AttackController;
+
+	[SerializeField]
+	float characterSpeed;
+
+    public Rigidbody2D rb;
+
+	//private ShapeController ShapeController { get; private set; }
+
+	#endregion
 
 
-    //private MovementController MovementController { get; private set; }
-    //private AttackController AttackController { get; private set; }
-    //private ShapeController ShapeController { get; private set; }
+	#region Public Manipulators
+	public void Move(InputAction.CallbackContext context)
+	{
+		movementController.Move(context);
+	}
 
-    #endregion
+	public void Jump(InputAction.CallbackContext context)
+	{
+		//TODO : Add A JUMP
+	}
+
+	public void Crouch(InputAction.CallbackContext context)
+	{
+		//TODO : Add a crouch
+	}
+
+	public void Attack(InputAction.CallbackContext context)
+	{
+		//TODO : Add an attack
+	}
+	#endregion
 
 
-    #region Public Manipulators
+	#region Inherited Manipulators
 
-    #endregion
-
-
-    #region Inherited Manipulators
-
-    void Awake()
+	void Awake()
     {
         // do not keep multiple instances
         if (Instance != null)
@@ -34,16 +59,23 @@ public class CharacterManager : MonoBehaviour
         }
 
         Instance = this;
+        movementController = gameObject.AddComponent<MovementController>();
+        AttackController = gameObject.AddComponent<AttackController>();
 
         CreateSubComponents();
     }
 
-    #endregion
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	#endregion
 
 
-    #region Private Manipulators
+	#region Private Manipulators
 
-    void CreateSubComponents()
+	void CreateSubComponents()
     {
 
     }
