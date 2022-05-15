3b24a006fc66a919ca8b13b545fbc17a3badf99d
ptilulu <lucas.moragues@wanadoo.fr>
2022-05-15 21:27:28 +0200

    [code] set the enum outside of the class (that was a mistake)

--- Assets/Script/Enums/CharacterShape.cs
+++ Assets/Script/Enums/CharacterShape.cs
@@ -1,22 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class CharacterShape
+/// <summary>
+/// Shapes that the main character can turn into
+/// </summary>
+public enum ECharacterShape
 {
-    #region Data Types
-
-    /// <summary>
-    /// Shapes that the main character can turn into
-    /// </summary>
-    public enum ECharacterShape
-    {
-        /// <summary> Default basic Human </summary>
-        Human,
+    /// <summary> Default basic Human </summary>
+    Human,
 
 
-        /// <summary> usefull for loops </summary>
-        count,
-    }
-    #endregion
+    /// <summary> usefull for loops </summary>
+    count,
 }
