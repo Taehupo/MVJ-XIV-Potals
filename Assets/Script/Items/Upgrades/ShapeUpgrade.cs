44c17f73c3924abbd21c90ddfbbbff4395904bd1
SupT <super-toad@hotmail.fr>
2022-06-12 16:23:25 +0200

    [code] Rat shape Upgrade item

---
+++ Assets/Script/Items/Upgrades/ShapeUpgrade.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeUpgrade : PickupItem
+{
+    [SerializeField] ECharacterShape associatedFlag = ECharacterShape.count;
+
+    private void Start()
+    {
+        // Check flag on GM
+        if (GameManager.instance.usableShapes.Contains(associatedFlag))
+            Destroy(gameObject);
+    }
+
+    public override void Effect(Collider2D collision)
+    {
+        GameManager.instance.AddShape(associatedFlag);
+        CharacterManager.Instance.SetShape(associatedFlag);
+    }
+
+}
