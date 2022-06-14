f4ae40999a35c0e00338f1bae403fc5d508300ce
Jercky <thejercky@gmail.com>
2022-06-14 22:45:15 +0200

    [ld] corrections

--- Assets/Script/Enums/EventFlag.cs
+++ Assets/Script/Enums/EventFlag.cs
@@ -8,7 +8,21 @@ public enum EEventFlag
 
     SuperPilumUnlocked,
 
-    DoorDiane1Unlocked,
+    DoorDiane01Unlocked,
+    DoorDiane02Unlocked,
+    DoorDiane03Unlocked,
+    DoorDiane04Unlocked,
+    DoorDiane05Unlocked,
+    DoorDiane06Unlocked,
+    DoorDiane07Unlocked,
+    DoorDiane08Unlocked,
+    DoorDiane09Unlocked,
+    DoorDiane10Unlocked,
+    DoorDiane11Unlocked,
+    DoorDiane12Unlocked,
+    DoorDiane13Unlocked,
+    DoorDiane14Unlocked,
+    DoorDiane15Unlocked,
 
     DianeBossDefeated,
 
