4e8ebeae4a5cb5196132f00bfa37505c274d92ae
Jercky <thejercky@gmail.com>
2022-12-04 17:34:48 +0100

    [fb] Timer avant que l'attraction des drops soit active

--- Assets/Script/Items/ItemTracker.cs
+++ Assets/Script/Items/ItemTracker.cs
@@ -7,10 +7,17 @@ public class ItemTracker : MonoBehaviour
     [SerializeField] string playerTag = "Player";
     [SerializeField] GameObject item;
     public float speed = 20.0f; 
+    private bool active = false;
+    private Timer timer;
 
+    private void Awake() {
+        timer = gameObject.AddComponent<Timer>();
+        timer.OnEnd = () => { active = true; };
+        timer.StartTimer(0.5f);
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag(playerTag))
+        if (collision.CompareTag(playerTag) && active)
         {
             Vector3 playerPosition = collision.gameObject.transform.position;
             
